1e637d935cf71287f410003e325b323a6073e24c
MiguelC <mcavalcante760@gmail.com>
2024-09-06 16:16:43 -0300

    update ip

--- OEEDash_V06.py
+++ OEEDash_V06.py
@@ -2,7 +2,7 @@ import streamlit as st
 import plotly.graph_objs as go
 import requests
 
-response = requests.get('http://192.168.104.238:5000/plc-data')
+response = requests.get('https://yj8ityf.localto.net/plc-data')
 plc_data = response.json()['plc_data']
 st.write(plc_data)
 
